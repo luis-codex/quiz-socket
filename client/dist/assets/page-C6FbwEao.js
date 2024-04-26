4605a6710c0b662b9e092285255520fc3b4634ac
Luis Tenorio <101147375+luis-tenorio-code@users.noreply.github.com>
2024-04-26 07:08:07 -0500

    Delete client/dist directory

--- client/dist/assets/page-C6FbwEao.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-C0czlPpn.js";function i(){return console.log("quizz"),o.jsx("div",{children:"quizz"})}export{i as default};
--- client/dist/assets/page-D6Agkauv.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-C0czlPpn.js";function r(){return console.log("results"),s.jsx("div",{children:"results"})}export{r as default};
